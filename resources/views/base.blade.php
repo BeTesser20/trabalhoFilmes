a4c8f908c343d6143fc848737ca7743571cb7bdf
BeTesser20 <beatrizptesser@gmail.com>
2024-09-10 08:59:42 -0300

    Modificação do base.blade

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,53 +1,43 @@
 <!DOCTYPE html>
 <html lang="en" class="astro-FLTEP2YP">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width">
-		<link rel="icon" type="image/svg+xml" href="/favicon.svg">
-		<meta name="generator" content="Astro v1.1.5">
-		<meta name="description" content="Template built with tailwindcss using Tailus blocks v2">
-		<title>Tailus astro theme</title>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg">
+	<meta name="generator" content="Astro v1.1.5">
+	<meta name="description" content="Template built with tailwindcss using Tailus blocks v2">
+	<title>Tailus astro theme</title>
 
-		<link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="/assets/index.350e2433.css" /></head>
-	<body class="bg-white dark:bg-gray-900 astro-FLTEP2YP">
-		<header class="astro-UY3JLCBK">
+	<link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="/assets/index.350e2433.css" />
+</head>
+<body class="bg-white dark:bg-gray-900 astro-FLTEP2YP">
+	<header class="astro-UY3JLCBK">
     <nav class="z-10 w-full absolute astro-UY3JLCBK">
-        <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
-    <div class="flex flex-wrap items-center justify-between py-2 gap-6 md:py-4 md:gap-0 relative astro-UY3JLCBK">
-                <input aria-hidden="true" type="checkbox" name="toggle_nav" id="toggle_nav" class="hidden peer astro-UY3JLCBK">
-                <div class="relative z-20 w-full flex justify-between lg:w-max md:px-0 astro-UY3JLCBK">
-                    <a href="#" aria-label="logo" class="flex space-x-2 items-center astro-UY3JLCBK">
-                        <div aria-hidden="true" class="flex space-x-1 astro-UY3JLCBK">
-                            <div class="h-4 w-4 rounded-full bg-gray-900 dark:bg-white astro-UY3JLCBK"></div>
-                            <div class="h-6 w-2 bg-primary astro-UY3JLCBK"></div>
-                        </div>
-                        <span class="text-2xl font-bold text-gray-900 dark:text-white astro-UY3JLCBK">Astrolus</span>
-                    </a>
-                    
-                    <div class="relative flex items-center lg:hidden max-h-10 astro-UY3JLCBK">
-                        <label role="button" for="toggle_nav" aria-label="humburger" id="hamburger" class="relative  p-6 -mr-6 astro-UY3JLCBK">
-                            <div aria-hidden="true" id="line" class="m-auto h-0.5 w-5 rounded bg-sky-900 dark:bg-gray-300 transition duration-300 astro-UY3JLCBK"></div>
-                            <div aria-hidden="true" id="line2" class="m-auto mt-2 h-0.5 w-5 rounded bg-sky-900 dark:bg-gray-300 transition duration-300 astro-UY3JLCBK"></div>
-                        </label>
-                    </div>
-                </div>
-                <div aria-hidden="true" class="fixed z-10 inset-0 h-screen w-screen bg-white/70 backdrop-blur-2xl origin-bottom scale-y-0 transition duration-500 peer-checked:origin-top peer-checked:scale-y-100 lg:hidden dark:bg-gray-900/70 astro-UY3JLCBK"></div>
-                <div class="flex-col z-20 flex-wrap gap-6 p-8 rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 justify-end w-full invisible opacity-0 translate-y-1  absolute top-full left-0 transition-all duration-300 scale-95 origin-top 
-                            lg:relative lg:scale-100 lg:peer-checked:translate-y-0 lg:translate-y-0 lg:flex lg:flex-row lg:items-center lg:gap-0 lg:p-0 lg:bg-transparent lg:w-7/12 lg:visible lg:opacity-100 lg:border-none
-                            peer-checked:scale-100 peer-checked:opacity-100 peer-checked:visible lg:shadow-none 
-                            dark:shadow-none dark:bg-gray-800 dark:border-gray-700 astro-UY3JLCBK">
-                   
-                    <div class="text-gray-600 dark:text-gray-300 lg:pr-4 lg:w-auto w-full lg:pt-0 astro-UY3JLCBK">
+      <div class="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+        <div class="flex flex-wrap items-center justify-between py-2 gap-6 md:py-4 md:gap-0 relative astro-UY3JLCBK">
+          
+          <div class="relative z-20 w-full flex justify-between lg:w-max md:px-0 astro-UY3JLCBK">
+            <a href="#" aria-label="logo" class="flex space-x-2 items-center astro-UY3JLCBK">
+              <div aria-hidden="true" class="flex space-x-1 astro-UY3JLCBK">
+                <div class="h-4 w-4 rounded-full bg-gray-900 dark:bg-white astro-UY3JLCBK"></div>
+                <div class="h-6 w-2 bg-primary astro-UY3JLCBK"></div>
+              </div>
+              <span class="text-2xl font-bold text-gray-900 dark:text-white astro-UY3JLCBK">LetterBoxd</span>
+            </a>
+          </div>
+
+          <div class="flex-col z-20 flex-wrap gap-6 p-8 rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 justify-end w-full invisible opacity-0 translate-y-1  absolute top-full left-0 transition-all duration-300 scale-95 origin-top lg:relative lg:scale-100 lg:peer-checked:translate-y-0 lg:translate-y-0 lg:flex lg:flex-row lg:items-center lg:gap-0 lg:p-0 lg:bg-transparent lg:w-7/12 lg:visible lg:opacity-100 lg:border-none peer-checked:scale-100 peer-checked:opacity-100 peer-checked:visible lg:shadow-none dark:shadow-none dark:bg-gray-800 dark:border-gray-700 astro-UY3JLCBK">
+                                <div class="text-gray-600 dark:text-gray-300 lg:pr-4 lg:w-auto w-full lg:pt-0 astro-UY3JLCBK">
                         <ul class="tracking-wide font-medium lg:text-sm flex-col flex lg:flex-row gap-6 lg:gap-0 astro-UY3JLCBK">
                             <li class="astro-UY3JLCBK">
-                                <a href="#" class="block md:px-4 transition hover:text-primary astro-UY3JLCBK">
-                                    <span class="astro-UY3JLCBK">Teams</span>
+                                <a href="{{route('index')}}" class="block md:px-4 transition hover:text-primary astro-UY3JLCBK">
+                                    <span class="astro-UY3JLCBK">Início</span>
                                 </a>
                             </li>
                             <li class="astro-UY3JLCBK">
-                                <a href="#" class="block md:px-4 transition hover:text-primary astro-UY3JLCBK">
-                                    <span class="astro-UY3JLCBK">Enterprise</span>
+                                <a href="{{route('filmes')}}" class="block md:px-4 transition hover:text-primary astro-UY3JLCBK">
+                                    <span class="astro-UY3JLCBK">Filmes</span>
                                 </a>
                             </li>
                             <li class="astro-UY3JLCBK">
@@ -69,15 +59,44 @@
                     </div>
 
                     <div class="mt-12 lg:mt-0 astro-UY3JLCBK">
-                        <a href="#" class="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max astro-UY3JLCBK">
+                        {{-- <a href="#" class="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max astro-UY3JLCBK">
                             <span class="relative text-sm font-semibold text-white astro-UY3JLCBK">Get Started</span>
-                        </a>
+                        </a> --}}
+                        
+                        
+                        
+                        {{--                         
+                        <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
+  
+                          <span class="flex items-center mr-2">
+                              @if (Auth::user())
+                                  {{ Auth::user()['nome'] }}
+                              @else
+                                  Você não está autenticado
+                              @endif
+                          </span>
+          
+                          <button @click="isOpen = !isOpen" class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-4 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
+                              <img src="{{ asset('img/user.jpg') }}">
+                          </button>
+                          <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
+                          <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
+                              @if(Auth::user())
+                                  <a href="{{route('logout')}}" class="block px-4 py-2 account-link hover:text-white">Logout</a>
+                              @else
+                                  <a href="{{route('login')}}" class="block px-4 py-2 account-link hover:text-white">Login</a>
+                              @endif
+                          </div>
+                        </div> --}}
+
+
                     </div>
-                </div>
             </div>
-</div>
+          </div>
+        </div>
+      </div>
     </nav>
-</header>
+  </header>
 		<main class="space-y-40 mb-40">
 		<div class="relative">
     <div aria-hidden="true" class="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
@@ -272,4 +291,5 @@
       </div>
 </div>
 </footer>
-	</body></html>
+</body>
+</html>
